ee4552d2434c22bc94d2e161bdc195247ca4887c
Michael <michael.ms.dev@gmail.com>
2025-02-21 12:32:44 -0300

    acrescentando a pasta js e nela 3 plugins, as mascaras, validações e mensagens de validações, criamos as validações para melhorar a interação e funcionalidade do formulário de contato

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('#carousel').slick({
         autoplay:true
     });
@@ -6,4 +6,42 @@ $(document).ready(function(){
     $('.menu-hamb').click(function() {
         $('nav').slideToggle();
     });
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(00) 00000-0000'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            veiculo: {
+                required: false,
+            },
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome',
+            telefone: 'Por favor, insira o seu telefone',
+            email: 'Por favor, insira o seu email',
+            veiculo: 'Por favor, nos diga qual é o veiculo de seu interesse',
+            mensagem: 'Por favor, digite sua mensagem acima'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos!`)
+            }
+        }
+    })
 });
